82f9874860a9209658e75e2f2212b8bf34fab0eb
vxp3503 <vikrantverma725@gmail.com>
2021-03-28 20:42:46 +0530

    DONE

---
+++ Bipertite_Graph_test.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Graph
+{
+    map<int,bool>visited;
+    map<int,list<int>>adj;
+    map<int,bool>color;
+    public:
+    void add_edge(int u,int v)
+    {
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    string bipertite(int node,int c)
+    {
+        visited[node]=1;
+        color[node]=c;
+        for(auto i=adj[node].begin();i!=adj[node].end();i++)
+        {
+            if(!visited[*i])
+            {
+                if(bipertite(*i,c^1)=="false")
+                return "false";
+            }
+            else if(color[node]==color[*i])
+            {
+                return "false";
+            }
+        }
+        return "true";
+    }
+};
+int main()
+{
+    Graph g1,g2;
+    g1.add_edge(1,2);
+    g1.add_edge(1,6);
+    g1.add_edge(6,5);
+    g1.add_edge(5,4);
+    g1.add_edge(3,4);
+    g1.add_edge(2,3);
+    cout<<g1.bipertite(1,0);
+
+    g2.add_edge(1,2);
+    g2.add_edge(2,3);
+    g2.add_edge(3,4);
+    g2.add_edge(2,4);
+    g2.add_edge(4,5);
+    cout<<endl<<g2.bipertite(1,0);
+}
